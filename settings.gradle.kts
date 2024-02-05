f4a90eba637d47dec7fc7b6a6bdfcd4e35b71169
Md Mehedi Hasan <145797601+mehediinf@users.noreply.github.com>
2024-02-06 03:28:47 +0600

    First Commit Cardview_Inside_Recyclerview

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Cardview_Inside_Recyclerview"
+include(":app")
+ 
